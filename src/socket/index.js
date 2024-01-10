65573eadad73291f8218a93c2dd51ab82ced96e4
guneyural <guneyural@gmail.com>
2024-01-10 13:03:09 +0300

    remove code repetition, move to functions

--- src/socket/index.js
+++ src/socket/index.js
@@ -9,51 +9,36 @@ const io = new Server();
 
 const stockMarkets = ["stockMarket1", "stockMarket2"];
 
+function joinStockMarket(socket, marketName) {
+  stockMarkets
+    .filter((market) => market !== marketName)
+    .forEach((item) => socket.leave(item));
+
+  socket.join(marketName);
+}
+
+function emitStockPrice(io, marketName, stockData, timer, interval) {
+  setInterval(() => {
+    if (timer < stockData.length) {
+      io.to(marketName).emit("listen stock prices", stockData[timer]);
+      timer++;
+    } else {
+      timer = 0;
+    }
+  }, interval);
+}
+
 io.on("connection", (socket) => {
   socket.on("join stock market 1", () => {
-    stockMarkets
-      .filter((market) => market != "stockMarket1")
-      .forEach((item) => socket.leave(item));
-
-    socket.join("stockMarket1");
+    joinStockMarket(socket, "stockMarket1");
   });
 
   socket.on("join stock market 2", () => {
-    stockMarkets
-      .filter((market) => market != "stockMarket2")
-      .forEach((item) => socket.leave(item));
-
-    socket.join("stockMarket2");
+    joinStockMarket(socket, "stockMarket2");
   });
 
-  let stockMarket1Timer = 0;
-  setInterval(() => {
-    if (stockMarket1Timer < stockMarket1.length) {
-      io.to("stockMarket1").emit(
-        "listen stock prices",
-        stockMarket1[stockMarket1Timer]
-      );
-
-      stockMarket1Timer++;
-    } else {
-      stockMarket1Timer = 0;
-    }
-  }, 1000);
-
-  let stockMarket2Timer = 0;
-
-  setInterval(() => {
-    if (stockMarket2Timer < stockMarket2.length) {
-      io.to("stockMarket2").emit(
-        "listen stock prices",
-        stockMarket2[stockMarket2Timer]
-      );
-
-      stockMarket2Timer++;
-    } else {
-      stockMarket2Timer = 0;
-    }
-  }, 1000);
+  emitStockPrice(io, "stockMarket1", stockMarket1, 0, 1000);
+  emitStockPrice(io, "stockMarket2", stockMarket2, 0, 1000);
 
   // Emit an event for broadcasting to connected user
   socket.on("stop listening all stock markets", () => {
@@ -62,15 +47,10 @@ io.on("connection", (socket) => {
 
   let timer = 0;
   setInterval(() => {
-    console.log({ timer });
     socket.emit("listen stock prices", otherValues[timer]);
 
-    if (timer + 1 >= otherValues.length) {
-      timer = 0;
-    } else {
-      timer += 1;
-    }
-  }, 1000);
+    timer = timer + 1 >= otherValues.length ? 0 : timer + 1;
+  }, 3000);
 
   socket.on("disconnect", () => {
     console.log("Disconnected");
